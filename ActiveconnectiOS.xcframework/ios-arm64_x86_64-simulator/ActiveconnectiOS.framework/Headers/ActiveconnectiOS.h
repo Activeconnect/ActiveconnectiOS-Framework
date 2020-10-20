3cc95b392a85053908088df6b7fa6e2198875d2c
Jason Mullings <jason@sonikpass.com>
2020-10-20 10:47:25 -0700

    Added framework

---
+++ ActiveconnectiOS.xcframework/ios-arm64_x86_64-simulator/ActiveconnectiOS.framework/Headers/ActiveconnectiOS.h
@@ -0,0 +1,19 @@
+//
+//  ActiveconnectiOS.h
+//  Activeconnect-ios
+//
+//  Created by Activeconnect on 2020-09-05.
+//  Copyright © 2020 Activeconnect Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for ActiveconnectiOS.
+FOUNDATION_EXPORT double ActiveconnectiOSVersionNumber;
+
+//! Project version string for ActiveconnectiOS.
+FOUNDATION_EXPORT const unsigned char ActiveconnectiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActiveconnectiOS/PublicHeader.h>
+
